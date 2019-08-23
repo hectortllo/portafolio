e388fa1d18000a05e889a96c6435a95d501a2e6e
hectortllo <hectortllo@gmail.com>
2019-08-22 21:21:00 -0600

    Integrated new and create actions for portafolios

--- app/controllers/portafolios_controller.rb
+++ app/controllers/portafolios_controller.rb
@@ -2,4 +2,20 @@ class PortafoliosController < ApplicationController
     def index
         @portfolio_items = Portafolio.all
     end
+
+    def new
+        @portfolio_item = Portafolio.new
+    end
+
+    def create
+        @portfolio_item = Portafolio.new(params.require(:portafolio).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @portfolio_item.save
+            format.html { redirect_to portafolios_path, notice: 'Blog was successfully created.' }
+          else
+            format.html { render :new }
+          end
+        end
+      end
 end
