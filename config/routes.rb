1fa88c92cf64784dff114ef3ea0fd6900c75951a
hectortllo <hectortllo@gmail.com>
2019-08-28 18:53:25 -0600

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   resources :portafolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+#  get 'pages/home'
+  #Se cambiaron las rutas para entrar a las diferentes páginas con una ruta más sencilla
+  get 'about-me', to: 'pages#about' # localhost:3000/about-me
+  get 'contact', to: 'pages#contact' #localhost:3000/contact
+  
+
+
+
   resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: 'pages#home' #Esta página será la página principal del sitio - localthost:3000/home
 end
