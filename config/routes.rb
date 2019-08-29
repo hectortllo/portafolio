811f872f82d365eecbda02faf615b607188df9ab
hectortllo <hectortllo@gmail.com>
2019-08-28 19:07:41 -0600

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :portafolios
+
+  #Las siguientes dos líneas de código se hacen para cambiar la ruta, al entrar a un solo
+  #portafolio, la ruta ya no dirá 'portafolios/3' sino, 'portafolio/3'. Se cambia de plural
+  #a singular. SE CREA UNA RUTA PROPIA.
+  #Al hacer esto también se cambió la ruta en el archivo index.htm.erb en la sección del título
+  #portfolio.item.title
+  resources :portafolios, except: [:show]
+  get 'portafolio/:id', to: 'portafolios#show', as: 'portfolio_show'
 
 #  get 'pages/home'
   #Se cambiaron las rutas para entrar a las diferentes páginas con una ruta más sencilla
