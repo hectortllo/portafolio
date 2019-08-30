ecfb1ad084f3e54b178ab78ac2b3b08fe19858d3
hectortllo <hectortllo@gmail.com>
2019-08-29 19:41:07 -0600

    Updated status for enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
-    extend FriendlyId
+  #enum es como una variable booleana, toma valor 1  o 0
+  enum status: { draft: 0, published: 1 }
+  extend FriendlyId
   friendly_id :title, use: :slugged
 end
