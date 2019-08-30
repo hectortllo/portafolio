ca459272ce105b5752b9acba4bf5f9609e0bb1f8
hectortllo <hectortllo@gmail.com>
2019-08-30 08:33:12 -0600

    Implemented image defaults for portfolio itemes

--- app/models/portafolio.rb
+++ app/models/portafolio.rb
@@ -8,4 +8,17 @@ class Portafolio < ApplicationRecord
 
     #Otra forma de hacer una consulta where
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails")}
+
+    #Después de crear (def create) este código se ejecutará
+    after_initialize :set_defaults
+
+    def set_defaults
+        #Básicamente ||= lo que hace es esto:
+        #if self.main_image == nil
+        #    self.main_image = "http://placehold.it/600x200"
+        #end
+        self.main_image ||= "http://placehold.it/600x200"
+        self.thumb_image ||= "http://placehold.it/350x200"
+    end
 end
+
