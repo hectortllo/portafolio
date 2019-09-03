cbe76c8bf17eb8274faeac301660f4177b8536b6
hectortllo <hectortllo@gmail.com>
2019-09-02 21:00:25 -0600

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #Validar que el atributo 'name' exista
+  validates_presence_of :name
+
+  def first_name
+    #Teniendo una cadena larga, tomar solo la primer palabra de esa cadena
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
