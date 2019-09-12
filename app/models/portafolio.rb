05229f45f307ad8aaf79ac898104f7e557dfb27f
hectortllo <hectortllo@gmail.com>
2019-09-11 18:56:17 -0600

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/portafolio.rb
+++ app/models/portafolio.rb
@@ -5,9 +5,8 @@ class Portafolio < ApplicationRecord
     accepts_nested_attributes_for :technologies, 
             reject_if: lambda { |attrs| attrs['name'].blank? } #Esta línea es para validar que el nombre no quede vacío
 
-    include Placeholder
-    validates_presence_of :title, :body, :main_image, :thumb_image
-
+    validates_presence_of :title, :body
+    
     mount_uploader :thumb_image, PortafolioUploader
     mount_uploader :main_image, PortafolioUploader
 
@@ -23,16 +22,6 @@ class Portafolio < ApplicationRecord
     #Otra forma de hacer una consulta where
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails")}
 
-    #Después de crear (def create) este código se ejecutará
-    after_initialize :set_defaults
-
-    def set_defaults
-        #Básicamente ||= lo que hace es esto:
-        #if self.main_image == nil
-        #    self.main_image = "http://placehold.it/600x200"
-        #end
-        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
+    #Después de crear (def create) este código se ejecutará    
 end
 
